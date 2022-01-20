29f8397ff313b53477073669aba882e272ec2997
Aksi <aksaule124026@gmail.com>
2022-01-20 18:49:26 +0600

    Begin

---
+++ check.go
@@ -0,0 +1,15 @@
+package main
+func main{
+
+	var a,b,c int
+	var a int
+
+	a := 15
+
+	var a int = 10
+
+	var a = 12
+
+	a = 11
+
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Println("Hello, Go!")
+//}
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("I like Go!")
+}
