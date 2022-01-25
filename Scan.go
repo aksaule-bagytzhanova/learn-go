20ce7fb1952d45114f3bd1c4a597751929692aa9
Aksi <aksaule124026@gmail.com>
2022-01-25 15:27:28 +0600

    Learn scan and string/int

---
+++ Scan.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int
+	var b int
+	var c int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+
+	a = a * a
+	b = b * b
+	c = a + b
+	fmt.Println(c)
+}
---
+++ String.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go"[0])
+	fmt.Println(string("Hello Go"[0]))
+}
